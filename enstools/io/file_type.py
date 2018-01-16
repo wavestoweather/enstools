d95ce840825c4541408a8548064a4b7debaf0e67
Robert Redl <robert.redl@lmu.de>
2018-01-16 15:34:44 +0100

    file type detection without libmagic/file. Fix for issue #17

--- enstools/io/file_type.py
+++ enstools/io/file_type.py
@@ -3,13 +3,10 @@ import logging
 import re
 import os
 
-__file_command = None
-__file_version = None
-
 
 def get_file_type(filename, only_extension=False):
     """
-    use libmagic to guess the type of input files
+    use the first bytes to guess the type of input files
 
     Parameters
     ----------
@@ -38,43 +35,22 @@ def get_file_type(filename, only_extension=False):
         if not os.path.exists(filename):
             raise IOError("file '%s' not found!" % filename)
 
-        # find the file command
-        global __file_command
-        global __file_version
-        if __file_command is None:
-            sts, __file_command = getstatusoutput("which file")
-            if sts != 0:
-                __file_command = None
-                __file_version = None
-                logging.warning("the 'file' command is not available; file types are estimated based on extension only!")
-            else:
-                sts, __file_version = getstatusoutput("%s -v" % __file_command)
-                try:
-                    __file_version = re.search("\d\.\d+", __file_version).group(0)
-                except AttributeError:
-                    __file_command = None
-                    __file_version = None
-                    logging.warning("unable to read the version of the file command; file types are estimated based on extension only!")
+        # read the first bytes and decide based on the content
+        with open(filename, "rb") as f:
+            first_bytes = f.read(12)
 
-        # use the file command to find the type
-        if __file_command is not None:
-            cmd = "%s -b %s" % (__file_command, filename)
-            sts, ft = getstatusoutput(cmd)
-            if sts != 0:
-                logging.warning("the command '%s' failed; file types are estimated based on extension only!" % cmd)
-            else:
-                if "Hierarchical Data Format" in ft:
-                    if file_type_based_on_extension is not None and "NC" in file_type_based_on_extension:
-                        file_type = "NC"
-                    else:
-                        file_type = "HDF"
-                if "Gridded binary (GRIB)" in ft:
-                    file_type = "GRIB"
-                elif "data" in ft:
-                    with open(filename, "rb") as f:
-                        first_bytes = f.read(12)
-                        if b"GRIB" in first_bytes:
-                            file_type = "GRIB"
+            # netcdf3 file
+            if first_bytes.startswith(b"CDF"):
+                file_type = "NC"
+            # netcdf4
+            elif b"HDF" in first_bytes[0:4]:
+                if file_type_based_on_extension is not None and "NC" in file_type_based_on_extension:
+                    file_type = "NC"
+                else:
+                    file_type = "HDF"
+            # grib
+            elif b"GRIB" in first_bytes:
+                file_type = "GRIB"
 
     # return the best knowledge of the file type
     if file_type is not None:
