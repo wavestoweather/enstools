907ca18101537da2eeac8ec4c76fb70c9a5b5abe
Robert Redl <robert.redl@lmu.de>
2018-03-18 16:50:47 +0100

    added possinility to run only python2 or python3 tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,15 +4,25 @@ set -e
 function usage {
     echo "arguments:"
     echo "-r    skip tests with R"
+    echo "-3    run only python3 tests"
+    echo "-2    run only python2 tests"
     exit -1
 }
 
 # parse the command line
 excluded_files=""
-while getopts "rh" opt ; do
+skip_python2=false
+skip_python3=false
+while getopts "rh23" opt ; do
     case $opt in
         r)
             excluded_files=test_scores_scoringRules_01.py
+            ;;
+        2)
+            skip_python3=true
+            ;;
+        3)
+            skip_python2=true
             ;;
         h)
             usage
@@ -24,15 +34,19 @@ if [[ ! -z $excluded_files ]] ; then
 fi
 
 # run all tests with python2 and python3
-echo "#################################################################################################################"
-echo "Running all tests with python 2 ...."
-echo "#################################################################################################################"
-echo
-nosetests --nocapture --nologcapture --with-doctest $ignore_option
+if [[ $skip_python2 == "false" ]] ; then
+    echo "#################################################################################################################"
+    echo "Running all tests with python 2 ...."
+    echo "#################################################################################################################"
+    echo
+    nosetests --nocapture --nologcapture --with-doctest $ignore_option
+fi
 
-echo
-echo "#################################################################################################################"
-echo "Running all tests with python 3 ...."
-echo "#################################################################################################################"
-echo
-nosetests3 --nocapture --nologcapture --with-doctest $ignore_option
+if [[ $skip_python3 == "false" ]] ; then
+    echo
+    echo "#################################################################################################################"
+    echo "Running all tests with python 3 ...."
+    echo "#################################################################################################################"
+    echo
+    nosetests3 --nocapture --nologcapture --with-doctest $ignore_option
+fi
