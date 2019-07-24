f0e3a18def2dda585882a30cce2dd9f543747300
B.Giebl <b.giebl@physik.uni-muenchen.de>
2019-07-24 20:29:40 +0200

    change content.py

--- enstools/misc/content.py
+++ enstools/misc/content.py
@@ -4,20 +4,32 @@ import numpy as np
 a = pandas.read_csv("content.log",delimiter="|", header=None,names=["file", "size", "time"])
 a = a[~a.file.str.contains("snow4")]
 a = a[~a.file.str.contains("content.log")]
-size = a["size"]
-time = a["time"]
+
 a["model"] = a["file"].apply(lambda x: x.split("/")[1])
 a["file_type"] = a["file"].apply(lambda x: x.split("/")[2])
 a["init_time"] = a["file"].apply(lambda x: x.split("/")[3])
 a["variable"] = a["file"].apply(lambda x: x.split("/")[4])
 a["filename"] = a["file"].apply(lambda x: x.split("/")[5])
 a["file"] = a["file"].apply(lambda x: "https://opendata.dwd.de/weather/nwp" +x[1:])
+
 a["level_type"] = a["filename"].apply(lambda x: x.split("_")[3])
+a["level_type"] = a["level_type"].apply(lambda x: x[:-6])
+
 a["forecast_time"] = a["filename"].apply(lambda x: x.split("_")[5])
-a["levels"] = 0
-a.loc[a["level_type"] == "singe-level",["levels"]] = 0
-a.loc[a["level_type"] == "pressure-level",["levels"]] = a[a.level_type == "pressure-level"]["filename"].apply(lambda x: x.split("_")[6])
-a.loc[a["level_type"] == "model-level",["levels"]] = a[a.level_type == "model-level"]["filename"].apply(lambda x: x.split("_")[6])
+a.loc[a["level_type"] == "time-inv",["forecast_time"]] = "0"
+a["forecast_time"] = a["forecast_time"].astype(int)
+
+a["levels"] = "0"
+a.loc[a["level_type"] == "single",["levels"]] = "0"
+a.loc[a["level_type"] == "pressure",["levels"]] = a[a.level_type == "pressure"]["filename"].apply(lambda x: x.split("_")[6])
+a.loc[a["level_type"] == "model",["levels"]] = a[a.level_type == "model"]["filename"].apply(lambda x: x.split("_")[6])
+a["levels"] = a["levels"].astype(int)
 
 
+
+# get variables of a init_time for a specific model:
+# a[a.model == "icon"][a.init_time == "00"]["variable"].drop_duplicates().values.tolist()
+# class content
+# init oben
+# 
 
