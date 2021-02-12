b9e48f0fbf5f7d20f3680d2307b4c81ece070030
Oriol Tintó <oriol.tinto@lmu.de>
2021-02-12 12:49:25 +0100

    Renaming few functions in io.compressor, io.analyzer

--- enstools/io/__init__.py
+++ enstools/io/__init__.py
@@ -5,5 +5,5 @@ from .file_type import get_file_type
 from .reader import read
 from .writer import write
 from .dataset import drop_unused
-from .compressor import compress
-from .analyzer import find_compression_parameters
+from .compressor import launch_compress_from_command_line, compress
+from .analyzer import launch_analysis_from_command_line, analyze
