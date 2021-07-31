61448e41414ead6a2987f468f8cc76bbb7359744
Robert Redl <robert.redl@lmu.de>
2021-07-31 11:49:10 +0200

    removed outdated some outdated code

--- enstools/post/__init__.py
+++ enstools/post/__init__.py
@@ -3,7 +3,5 @@ Post-Processing routines
 """
 
 from .ConvectiveAdjustmentTimescale import convective_adjustment_time_scale
-from .RossbyWavePacketsDiagnostics import rossby_wave_packets_diag
-from .RossbyWavePacketsDiagnostics import rossby_wave_packets_plot
 from .EnsembleStat import ensemble_stat
 
