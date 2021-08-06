8391a564c0360ddd5c14b51764b5784f1e6bb8ba
Oriol Tintó <oriol.tinto@lmu.de>
2021-08-06 10:36:54 +0200

    Fixinf the synthetic dataset generator. Adding more tests

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -1,6 +1,6 @@
 from os.path import isfile, join
 import pytest
-from enstools.io.encoding import check_sz_availability
+from enstools.io.encoding import check_sz_availability, check_libpressio_availability
 
 def create_synthetic_dataset(directory):
     """
@@ -13,30 +13,20 @@ def create_synthetic_dataset(directory):
     import pandas as pd
     from scipy.ndimage import gaussian_filter
     # Create synthetic datasets
-    nx, ny, nz, t = 100, 100, 100, 10
-    lons = np.linspace(-180, 180, nx)
-    lats = np.linspace(-90, 90, ny)
-    lon, lat = np.meshgrid(lons, lats)
+    nx, ny, nz, t = 360, 91, 31, 10
+    lon = np.linspace(-180, 180, nx)
+    lat = np.linspace(-90, 90, ny)
     levels = np.array(range(nz))
     for dimension in [1, 2, 3]:
         if dimension == 1:
-            data_size = (nx, t)
-            var_dimensions = ["x", "time"]
-            coord_dimensions = ["x"]
-            coord_lon = lons
-            coord_lat = lats
+            data_size = (t, nx)
+            var_dimensions = ["time", "lon"]
         elif dimension == 2:
-            data_size = (nx, ny, t)
-            var_dimensions = ["x", "y", "time"]
-            coord_dimensions = ["x", "y"]
-            coord_lon = lon
-            coord_lat = lat
+            data_size = (t, nx, ny)
+            var_dimensions = ["time", "lon", "lat"]
         elif dimension == 3:
-            data_size = (nx, ny, nz, t)
-            var_dimensions = ["x", "y", "z", "time"]
-            coord_dimensions = ["x", "y"]
-            coord_lon = lon
-            coord_lat = lat
+            data_size = (t,nz, nx, ny)
+            var_dimensions = ["time", "level", "lon", "lat"]
         else:
             raise NotImplementedError()
 
@@ -52,15 +42,16 @@ def create_synthetic_dataset(directory):
             },
 
             coords={
-                "lon": (coord_dimensions, coord_lon),
-                "lat": (coord_dimensions, coord_lat),
-                "level": ("z", levels),
+                "lon": lon,
+                "lat": lat,
+                "level": levels,
                 "time": pd.date_range("2014-09-06", periods=t),
                 "reference_time": pd.Timestamp("2014-09-05"),
             },
         )
         ds_name = "dataset_%iD.nc" % dimension
         ds.to_netcdf(join(directory, ds_name))
+
 
 
 def file_size(file_path):
@@ -101,6 +92,7 @@ class TestClass:
         cls.input_tempdir = TempDir(check_free_space=False)
         cls.output_tempdir = TempDir(check_free_space=False)
         create_synthetic_dataset(cls.input_tempdir.getpath())
+
     @classmethod
     def teardown_class(cls):
         # release resources
@@ -116,7 +108,38 @@ class TestClass:
         for ds in datasets:
             assert isfile(join(tempdir_path, ds))
 
+    def test_analyzer(self):
+        from enstools.io import analyze
+        input_tempdir = self.input_tempdir
+        # Check that the compression without specifying compression parameters works
+        datasets = ["dataset_%iD.nc" % dimension for dimension in range(1, 4)]
+        for ds in datasets:
+            input_path = join(input_tempdir.getpath(), ds)
+            analyze(file_paths=[input_path])
 
+    def test_zfp_analyzer(self):
+        from enstools.io import analyze
+        input_tempdir = self.input_tempdir
+        # Check that the compression without specifying compression parameters works
+        datasets = ["dataset_%iD.nc" % dimension for dimension in range(1, 4)]
+        for ds in datasets:
+            input_path = join(input_tempdir.getpath(), ds)
+            analyze(file_paths=[input_path], compressor="zfp")
+
+    def test_sz_analyzer(self):
+        from enstools.io import analyze
+        input_tempdir = self.input_tempdir
+        # Check that the compression without specifying compression parameters works
+        datasets = ["dataset_%iD.nc" % dimension for dimension in range(1, 4)]
+        for ds in datasets:
+            input_path = join(input_tempdir.getpath(), ds)
+            analyze(file_paths=[input_path], compressor="sz")
+
+    @pytest.mark.skipif(check_sz_availability(), reason="Requires SZ not being available")
+    def test_sz_checker(self):
+        compression = "lossy:sz:pw_rel:0.1"
+        with pytest.raises(AssertionError):
+            wrapper(self, compression=compression) 
 
     def test_compress_vanilla(self):
         wrapper(self)
@@ -134,14 +157,35 @@ class TestClass:
         compression = "lossy:sz:pw_rel:0.1"
         wrapper(self, compression=compression)
 
+    @pytest.mark.skipif( not check_sz_availability(), reason="Requires SZ")
+    def test_compress_sz_abs(self):
+        compression = "lossy:sz:abs:0.01"
+        wrapper(self, compression=compression)
+
+    @pytest.mark.skipif( not check_sz_availability(), reason="Requires SZ")
+    def test_compress_sz_rel(self):
+        compression = "lossy:sz:rel:0.001"
+        wrapper(self, compression=compression)
+
+
     def test_compress_zfp_vanilla(self):
         compression = "lossy:zfp"
         wrapper(self, compression=compression)
 
-    def test_compress_zfp_rate_1(self):
+    def test_compress_zfp_rate(self):
         compression = "lossy:zfp:rate:1"
         wrapper(self, compression=compression)
-        
+    
+
+    def test_compress_zfp_accuracy(self):
+        compression = "lossy:zfp:accuracy:.1"
+        wrapper(self, compression=compression)
+
+    def test_compress_zfp_precision(self):
+        compression = "lossy:zfp:precision:17"
+        wrapper(self, compression=compression)
+    
+
     def test_compress_json_parameters(self):
         input_tempdir = self.input_tempdir
         import json
@@ -172,13 +216,10 @@ class TestClass:
         compression = yaml_file_path
         wrapper(self, compression=compression)
         
-
-
     def test_compress_auto(self):
         compression="auto"
         wrapper(self, compression=compression)
 
-    """
     def test_compress_ratios_lossy(self):
         from enstools.io import compress
         input_tempdir = self.input_tempdir
@@ -194,8 +235,7 @@ class TestClass:
             initial_size = file_size(input_path)
             final_size = file_size(output_file_path)
             assert initial_size > final_size
-    """
-    
+
     def test_compress_ratios_lossless(self):
         from enstools.io import compress
         # Check that compression works when specifying compression = lossy:sz
@@ -212,7 +252,8 @@ class TestClass:
             initial_size = file_size(input_path)
             final_size = file_size(output_file_path)
             assert initial_size > final_size
-        
+    
+
     #def test_compress_multinode(self):
     #    # Check that compression works when specifying compression = lossy:sz
     #    input_tempdir = self.input_tempdir
@@ -241,11 +282,4 @@ class TestClass:
         from enstools.io.encoding import check_zfp_availability
         assert check_zfp_availability
 
-    """
-    # For now we keep this test disabled since we are not able to easily provide the sz package
-
-    def test_sz_filter_availability(self):
-        from enstools.io.encoding import check_sz_availability
-        assert check_sz_availability
-    """
 
