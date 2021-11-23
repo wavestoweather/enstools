d1f4dd7ae945a263889e94c80ba8d4d903b5532f
Oriol Tintó <oriol.tinto@lmu.de>
2021-11-23 17:31:14 +0100

    Fix check_compression ratios and rename output_folder variable.

--- enstools/io/compressor.py
+++ enstools/io/compressor.py
@@ -53,13 +53,13 @@ def transfer(file_paths: Union[List[str], str], output: str, compression: str = 
         assert isdir(output), "For multiple files, the output parameter should be a directory"
         transfer_multiple_files(
                                 file_paths=file_paths,
-                                output_folder=output,
+                                output=output,
                                 compression=compression,
                                 variables_to_keep=variables_to_keep,
                                 )
 
 
-def transfer_multiple_files(file_paths: List[str], output_folder: str, compression: str = "lossless",
+def transfer_multiple_files(file_paths: Union[List[str],str], output: str, compression: str = "lossless",
                             variables_to_keep: List[str] = None):
     from dask import compute
     # Create and fill the list of tasks
@@ -69,7 +69,7 @@ def transfer_multiple_files(file_paths: List[str], output_folder: str, compressi
     temporary_names_dictionary = {}
 
     for file_path in file_paths:
-        new_file_path = destination_path(file_path, output_folder)
+        new_file_path = destination_path(file_path, output)
 
         # Create temporary file name and store it in the dictionary
         temporary_file_path = f"{new_file_path}.tmp"
@@ -147,7 +147,7 @@ def destination_path(origin_path: str, destination_folder: str):
     return destination
 
 
-def compress(file_paths: List[str], output_folder: str,  compression: str, nodes: int = 0,
+def compress(file_paths: Union[List[str], str], output: str, compression: str, nodes: int = 0,
              variables_to_keep: List[str] = None):
     """
     Copies a list of files to a destination folder, optionally applying compression.
@@ -157,7 +157,7 @@ def compress(file_paths: List[str], output_folder: str,  compression: str, nodes
     if compression == "auto":
         from .analyzer import analyze
         import os
-        compression_parameters_path = os.path.join(output_folder, "compression_parameters.yaml")
+        compression_parameters_path = os.path.join(output, "compression_parameters.yaml")
         # By using thresholds = None we will be using the default values.
         analyze(file_paths, thresholds=None, output_file=compression_parameters_path)
         # Now lets continue setting compression = compression_parameters_path
@@ -174,28 +174,49 @@ def compress(file_paths: List[str], output_folder: str,  compression: str, nodes
                 # Transfer will copy the files from its origin path to the output folder,
                 # using read and write functions from enstools
                 init_time = time.time()
-                transfer(file_paths, output_folder, compression, variables_to_keep=variables_to_keep)
+                transfer(file_paths, output, compression, variables_to_keep=variables_to_keep)
 
     else:
         # Transfer will copy the files from its origin path to the output folder,
         # using read and write functions from enstools
         init_time = time.time()
-        transfer(file_paths, output_folder, compression, variables_to_keep=variables_to_keep)
+        transfer(file_paths, output, compression, variables_to_keep=variables_to_keep)
 
-    # We could compute compression ratios
+    end_time = time.time()
+
+    check_compression_ratios(file_paths, output)
+
+    return end_time - init_time
+
+
+def check_compression_ratios(file_paths: Union[List[str], str], output: str):
+    # Compute compression ratios
     from .metrics import compression_ratio
     from os.path import join, basename
     from pprint import pprint
     compression_ratios = {}
+
+    # Single file case
+    if isinstance(file_paths, str):
+        file_path = file_paths
+        if isdir(output):
+            file_name = basename(file_path)
+            new_file_path = join(output, file_name)
+        elif isfile(output):
+            new_file_path = output
+        CR = compression_ratio(file_path, new_file_path)
+        print(f"Compression ratios after compression:\nCR: {CR:.1f}")
+        return
+
+    # Multiple files
     for file_path in file_paths:
         file_name = basename(file_path)
         file_name = fix_filename(file_name)
-        new_file_path = join(output_folder, file_name)
+        new_file_path = join(output, file_name)
         print(new_file_path, isfile(new_file_path))
         if isfile(new_file_path):
             CR = compression_ratio(file_path, new_file_path)
             compression_ratios[basename(file_path)] = CR
     print("Compression ratios after compression:")
     pprint(compression_ratios)
-    end_time = time.time()
-    return end_time - init_time
+
