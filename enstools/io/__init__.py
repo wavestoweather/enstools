d0dcbcc5132fc8359db47932a89e8e51d41b2056
Oriol Tintó Prims <oriol.tinto@lmu.de>
2021-12-17 16:10:09 +0100

    Changed few imports to adapt to the new structure.

--- enstools/io/__init__.py
+++ enstools/io/__init__.py
@@ -2,6 +2,7 @@
 Reading and Writing of meteorological data
 """
 
+
 def __clean_HDF5_PLUGIN_PATH():
     """
     if the libraries from hdf5plugin are in HDF5_PLUGIN_PATH, then remove them
@@ -23,6 +24,7 @@ def __clean_HDF5_PLUGIN_PATH():
         else:
             del os.environ["HDF5_PLUGIN_PATH"]
 
+
 # TODO: figure out why this is needed and remove it!
 __clean_HDF5_PLUGIN_PATH()
 
@@ -30,7 +32,8 @@ from .file_type import get_file_type
 from .reader import read
 from .writer import write
 from .dataset import drop_unused
-from .compressor import compress
-from .analyzer import analyze
+from enstools.io.compression.compressor import compress
+from enstools.io.compression.analyzer import analyze
+from enstools.io.compression.evaluator import evaluate
 from .cli import main as cli
-from .evaluator import evaluate
+
