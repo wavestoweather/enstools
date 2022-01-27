ddc563a273c96cbf83c7e5c8cfd362c6b55ebde5
Oriol Tintó <oriol.tinto@lmu.de>
2022-01-27 14:49:32 +0100

    Adding pruner to enstools.io.compression init file.

--- enstools/io/compression/__init__.py
+++ enstools/io/compression/__init__.py
@@ -2,3 +2,4 @@ from .compressors.availability_checks import check_zfp_availability, check_blosc
                                              check_filter_availability, check_libpressio_availability, \
                                              check_all_filters_availability, check_compression_filters_availability
 from .encoding import define_encoding, set_compression_attributes
+from .pruner import pruner
