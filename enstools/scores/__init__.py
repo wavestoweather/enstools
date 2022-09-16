947c950d09f8b40de1275fce0c71a4e3e830e1cc
Oriol Tintó <oriol.tinto@lmu.de>
2022-09-16 14:19:44 +0200

    Add function add_score_from_file in enstools.scores

--- enstools/scores/__init__.py
+++ enstools/scores/__init__.py
@@ -1,6 +1,7 @@
 """
 Routines for Evaluation of model forecasts
 """
+from xxlimited import foo
 from .DisplacementAmplitudeScore import das
 
 from .continuous_ranked_probability_score import continuous_ranked_probability_score
@@ -21,3 +22,32 @@ ssim_I = structural_similarity_log_index
 correlation_I = pearson_correlation_index
 nrmse_I = normalized_root_mean_square_error_index
 ks_I = kolmogorov_smirnov_index
+
+
+def add_score_from_file(file_path: str):
+    """
+    Read a python file and add the function with the same name as the file to the available scores in enstools.scores
+    """
+
+    import importlib
+    import os
+    module_name = os.path.basename(file_path).strip(".py")
+    
+    import importlib.util
+    import sys
+
+    # Get spec from the file path
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+
+    # Get the module
+    mod = importlib.util.module_from_spec(spec)
+    
+    # Load the module
+    spec.loader.exec_module(mod)
+
+    # Get the function
+    function = mod.__getattribute__(module_name)
+
+    # Add the function as a new score in module enstools.scores
+    setattr(sys.modules[__name__], module_name, function)
+    
