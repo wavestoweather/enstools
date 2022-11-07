2a8489c66d5ded5502a1441d7cc40211b234e512
Oriol Tintó <oriol.tinto@lmu.de>
2022-11-07 16:16:34 +0100

    Removing engine="h5netcdf" from read function.

--- enstools/scores/__init__.py
+++ enstools/scores/__init__.py
@@ -1,9 +1,12 @@
 """
 Routines for Evaluation of model forecasts
 """
-from xxlimited import foo
-from .DisplacementAmplitudeScore import das
+# Imports for typing
+from typing import Callable
+from xarray import DataArray
 
+# Import actual scores
+from .DisplacementAmplitudeScore import das
 from .continuous_ranked_probability_score import continuous_ranked_probability_score
 from .kolmogorov_smirnov import kolmogorov_smirnov, kolmogorov_smirnov_index
 from .kolmogorov_smirnov_multicell import kolmogorov_smirnov_multicell
@@ -22,6 +25,7 @@ ssim_I = structural_similarity_log_index
 correlation_I = pearson_correlation_index
 nrmse_I = normalized_root_mean_square_error_index
 ks_I = kolmogorov_smirnov_index
+psnr = peak_signal_to_noise_ratio
 
 
 def add_score_from_file(file_path: str):
@@ -32,7 +36,7 @@ def add_score_from_file(file_path: str):
     import importlib
     import os
     module_name = os.path.basename(file_path).strip(".py")
-    
+
     import importlib.util
     import sys
 
@@ -41,13 +45,20 @@ def add_score_from_file(file_path: str):
 
     # Get the module
     mod = importlib.util.module_from_spec(spec)
-    
+
     # Load the module
     spec.loader.exec_module(mod)
 
     # Get the function
     function = mod.__getattribute__(module_name)
 
+    register_score(function, module_name)
+
+
+def register_score(function: Callable[[DataArray, DataArray], DataArray], name: str):
+    """
+    Register a given function as a score.
+    """
+    import sys
     # Add the function as a new score in module enstools.scores
-    setattr(sys.modules[__name__], module_name, function)
-    
+    setattr(sys.modules[__name__], name, function)
