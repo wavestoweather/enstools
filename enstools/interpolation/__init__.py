88edca63ac6601fb3c8f73304f2d86a2213435ab
Oriol Tintó <oriol.tinto@lmu.de>
2022-11-11 10:06:33 +0100

    Removing outdated interpolation_unstagger and unstagger_grid.py.

--- enstools/interpolation/__init__.py
+++ enstools/interpolation/__init__.py
@@ -1,4 +1,3 @@
 from .coarse_graining import downsize
 from .nearest_neighbour_interpolator import nearest_neighbour
 from .vertical_interpolation import model2pressure
-from .unstagger_grid import unstagger
